9497150910ac6bb25eda24c9e39049f9ce5089fa
michalwachowicz <wachowiczmichal1@gmail.com>
2024-07-13 17:53:38 +0200

    Register buttons click actions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const btnContainer = document.querySelector(".btn-container");
 const screen = document.querySelector(".screen");
 
 const operate = (a, operator, b) => {
@@ -14,3 +15,41 @@ const operate = (a, operator, b) => {
       return "Error";
   }
 };
+
+btnContainer.addEventListener("click", (e) => {
+  const target = e.target;
+  if (!target || target.type != "submit") return;
+
+  // Digit button clicked
+  if (target.classList.contains("digit-btn")) {
+    screen.textContent += target.textContent;
+  }
+
+  // Equals button clicked
+  if (target.classList.contains("equals-btn")) {
+  }
+
+  // Clear button clicked
+  if (target.id == "clear") {
+  }
+
+  // Back button clicked
+  if (target.id == "back") {
+  }
+
+  // Divide button clicked
+  if (target.id == "divide") {
+  }
+
+  // Multiply button clicked
+  if (target.id == "multiply") {
+  }
+
+  // Add button clicked
+  if (target.id == "add") {
+  }
+
+  // Subtract button clicked
+  if (target.id == "subtract") {
+  }
+});
